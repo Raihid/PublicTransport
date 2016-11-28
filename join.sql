39f7ab834ba7c2f68b472a2b65a65447510db2c4
Maciej Wołczyk <maciej.wolczyk@gmail.com>
2016-11-28 19:06:41 +0100

    Added joins and union

--- join.sql
+++ join.sql
@@ -1,1 +1,4 @@
 SELECT station.*, stop.* FROM station INNER JOIN stop ON stop.station = station.id;
+SELECT station.*, stop.* FROM station LEFT JOIN stop ON stop.station = station.id;
+SELECT station.*, stop.* FROM station RIGHT JOIN stop ON stop.station = station.id;
+SELECT * FROM drivers UNION ALL SELECT * FROM vehicles;
